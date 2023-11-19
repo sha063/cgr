1363290a1e73b3fad4e1ef7c7924da953b2f8010
sha063 <shihabaaqil22242224@gmail.com>
2023-11-19 17:30:01 +0530

    1

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+import datetime as dt
+import os
 
-# Create your models here.
+def getFileName(request,filename):
+    now = dt.datetime.now().strftime('%Y%m%d%H:%S')
+    new_filename = "%s%s"%(now,filename)
+    return os.path.join('uploads/',new_filename)
+
+class Category(models.Model):
+    name = models.CharField(max_length=150,null=False,blank=False)
+    image = models.ImageField(upload_to=getFileName,null=True, blank=True)
+    description = models.TextField(max_length=500,null=False,blank=False)
+    status = models.BooleanField(default=False,help_text="0-default,1-hidden")
+    trending = models.BooleanField(default=False,help_text="0-default,1-trending")
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+        
+class Product(models.Model):
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    name = models.CharField(max_length=150,null=False,blank=False)
+    image = models.ImageField(upload_to=getFileName,null=True,blank=True)
+    description = models.TextField(max_length=500,null=False,blank=False)
+    quantity = models.IntegerField(null=False,blank=False)
+    original_price = models.FloatField(null=False,blank=False)
+    selling_price = models.FloatField(null=False,blank=False)
+    trending = models.BooleanField(default=False,help_text="0-default,1-trending")
+    created_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name  
+    
+class User(models.Model):
+    name = models.CharField(max_length=150,null=False,blank=False)
+    image = models.ImageField(upload_to=getFileName,null=True,blank=True)
+    password = models.CharField(max_length=20)
+    
+    def __str__(self):
+        return self.name  
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,121 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from .models import *
+from django.db import models
+from django.contrib.auth.decorators import login_required
 
+#links
+Login = 'Login'
+Profile = 'Profile'
+PagesFaq = 'PagesFaq'
+Home = 'Home'
+Signup = 'Signup'
+class profile_header:
+    def __init__(self, icon, name,link):
+        self.icon = icon
+        self.name = name
+        self.link = link
+
+ProfileHeader = [profile_header('person','My Profile',Profile),
+     #profile_header('gear','Account Settings','settings'),
+     profile_header('question-circle','Need Help?',PagesFaq),
+     profile_header('box-arrow-right','Sign Out',Login),]
+
+#@login_required
+def dashboard(request):
+   template = loader.get_template('registration/dashboard.html')
+   context = {
+        'section':'dashboard',
+        'title': 'CGR',
+        'page_title': Home,
+        'notifications': ['red', 'yellow', 'green'],
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'users':User.objects.all(),
+        'profile_header':ProfileHeader,
+    }
+   return HttpResponse(template.render(context, request)) 
+
+#@login_required
 def home(request):
-    return render(request,"shop/index.html")
+    template = loader.get_template('shop/home.html')
+    context = {
+
+        'title': 'CGR',
+        'page_title': Home,
+        'notifications': ['red', 'yellow', 'green'],
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'users':User.objects.all(),
+        'profile_header':ProfileHeader,
+    }
+    return HttpResponse(template.render(context, request))
+
+def profile(request):
+    template = loader.get_template('shop/profile.html')
+    context = {
+        'page_title': Profile,
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'user':User.objects.all(),
+        'profile_header':ProfileHeader,
+    }
+    return HttpResponse(template.render(context, request))
+
+def settings(request):
+    template = loader.get_template('shop/settings.html')
+    context = {
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'user': Product.objects.all(),
+        'profile_header':ProfileHeader,
+    }
+    return HttpResponse(template.render(context, request))
+
+def pages_faq(request):
+    template = loader.get_template('shop/pages_faq.html')
+    context = {
+        'page_title':PagesFaq,
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'user': Product.objects.all(),
+        'profile_header':ProfileHeader,
+    }
+    return HttpResponse(template.render(context, request))
+
+def signup(request):
+    template = loader.get_template('shop/signup.html')
+    context = {
+        'page_title':PagesFaq,
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'user': Product.objects.all(),
+        'profile_header':ProfileHeader,
+    }
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    template = loader.get_template('shop/login.html')
+    context = {
+        'page_title':PagesFaq,
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'user': Product.objects.all(),
+        'profile_header':ProfileHeader,
+    }
+    return HttpResponse(template.render(context, request))
+
+def testing(request):
+    template = loader.get_template('shop/testing.html')
+    context = {
+        'category': Category.objects.all(),
+        'product': Product.objects.all(),
+        'user': Product.objects.all(),
+        'profile_header':a,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+''' def testing(request):
+    return render(request, 'shop/testing.html', {'category': Category.objects.all()}) '''
